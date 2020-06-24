d2bb41e445f0fc9dc0cfef53fc5c89cd5d0b0743
Phodal Huang <h@phodal.com>
2020-06-24 13:10:06 +0800

    clone: add basic key event for quit

--- src/rinput/editor.rs
+++ src/rinput/editor.rs
@@ -44,8 +44,6 @@ impl Editor {
         };
 
         buffers.push(Arc::new(Mutex::new(buffer)));
-
-        println!("............");
         let view = View::new(buffers[0].clone(), width, height);
 
         Editor {
@@ -64,10 +62,41 @@ impl Editor {
         self.view.draw(&mut self.rb);
     }
 
+    /// Handle key events
+///
+/// Key events can be handled in an Overlay, OR in the current Mode.
+///
+/// If there is an active Overlay, the key event is sent there, which gives
+/// back an OverlayEvent. We then parse this OverlayEvent and determine if
+/// the Overlay is finished and can be cleared. The response from the
+/// Overlay is then converted to a Command and sent off to be handled.
+///
+/// If there is no active Overlay, the key event is sent to the current
+/// Mode, which returns a Command which we dispatch to handle_command.
+    fn handle_key_event(&mut self, event: Event) {
+        let key = Key::from_event(&mut self.rb, event);
+
+        let key = match key {
+            Some(k) => k,
+            None => return
+        };
+
+        // println!("{}", key);
+        self.running = false
+    }
+
     pub fn start(&mut self) {
         while self.running {
             self.draw();
             self.rb.present();
+
+            match self.rb.poll_event(true) {
+                Ok(Event::ResizeEvent(width, height)) => {
+
+                },
+                Ok(key_event) => self.handle_key_event(key_event),
+                _ => {}
+            }
         }
     }
 }
--- src/rinput/keyboard.rs
+++ src/rinput/keyboard.rs
@@ -1,3 +1,6 @@
+use std::char;
+use std::time::Duration;
+
 use rustbox::{RustBox, Event};
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
@@ -23,7 +26,69 @@ pub enum Key {
 impl Key {
     pub fn from_special_code(code: u16) -> Option<Key> {
         match code {
-            _ => None,
+            1     => Some(Key::Ctrl('a')),
+            2     => Some(Key::Ctrl('b')),
+            3     => Some(Key::Ctrl('c')),
+            4     => Some(Key::Ctrl('d')),
+            5     => Some(Key::Ctrl('e')),
+            6     => Some(Key::Ctrl('f')),
+            7     => Some(Key::Ctrl('g')),
+            8     => Some(Key::Ctrl('h')),
+            9     => Some(Key::Tab),
+            13    => Some(Key::Enter),
+            14    => Some(Key::Ctrl('n')),
+            16    => Some(Key::Ctrl('p')),
+            17    => Some(Key::Ctrl('q')),
+            18    => Some(Key::Ctrl('r')),
+            19    => Some(Key::Ctrl('s')),
+            24    => Some(Key::Ctrl('x')),
+            25    => Some(Key::Ctrl('y')),
+            26    => Some(Key::Ctrl('z')),
+            27    => Some(Key::Esc),
+            32    => Some(Key::Char(' ')),
+            127   => Some(Key::Backspace),
+            65514 => Some(Key::Right),
+            65515 => Some(Key::Left),
+            65516 => Some(Key::Down),
+            65517 => Some(Key::Up),
+            65520 => Some(Key::End),
+            65521 => Some(Key::Home),
+            65522 => Some(Key::Delete),
+            _     => None,
+        }
+    }
+
+    pub fn from_chord(rb: &mut RustBox, start: u16) -> Option<Key> {
+        let chord = Key::get_chord(rb, start);
+
+        match chord.as_str() {
+            "\x1b[1;5C" => Some(Key::CtrlRight),
+            "\x1b[1;5D" => Some(Key::CtrlLeft),
+            _ => Key::from_special_code(start)
+        }
+    }
+
+    pub fn get_chord(rb: &mut RustBox, start: u16) -> String {
+        // Copy any data waiting to a string
+        // There may be a cleaner way to do this?
+        let mut chord = char::from_u32(u32::from(start)).unwrap().to_string();
+        while let Ok(Event::KeyEventRaw(_, _, ch)) = rb.peek_event(Duration::from_secs(0), true) {
+            chord.push(char::from_u32(ch).unwrap())
+        }
+
+        chord
+    }
+
+    pub fn from_event(rb: &mut RustBox, event: Event) -> Option<Key> {
+        match event {
+            Event::KeyEventRaw(_, k, ch) => {
+                match k {
+                    0 => char::from_u32(ch).map(Key::Char),
+                    0x1b => Key::from_chord(rb, 0x1b),
+                    a => Key::from_special_code(a)
+                }
+            },
+            _ => None
         }
     }
 }
